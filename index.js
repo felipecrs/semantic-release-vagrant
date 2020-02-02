b6107f4649776c6c27d83fcd159d4cbe7bacf60b
Felipe Santos <felipecassiors@gmail.com>
2020-02-02 01:02:20 -0300

    chore: create project based on @semantic-release/apm plugin

---
+++ index.js
@@ -0,0 +1,68 @@
+/* eslint require-atomic-updates: off */
+
+const AggregateError = require('aggregate-error');
+const getPkg = require('./lib/get-pkg');
+const verifyApm = require('./lib/verify');
+const prepareApm = require('./lib/prepare');
+const publishApm = require('./lib/publish');
+
+let verified;
+let prepared;
+
+async function verifyConditions(pluginConfig, context) {
+  const errors = await verifyApm(pluginConfig, context);
+
+  try {
+    await getPkg(pluginConfig, context);
+  } catch (error) {
+    errors.push(...error);
+  }
+
+  if (errors.length > 0) {
+    throw new AggregateError(errors);
+  }
+
+  verified = true;
+}
+
+async function prepare(pluginConfig, context) {
+  const errors = verified ? [] : await verifyApm(pluginConfig, context);
+
+  try {
+    await getPkg(pluginConfig, context);
+  } catch (error) {
+    errors.push(...error);
+  }
+
+  if (errors.length > 0) {
+    throw new AggregateError(errors);
+  }
+
+  await prepareApm(pluginConfig, context);
+
+  prepared = true;
+}
+
+async function publish(pluginConfig, context) {
+  let pkg;
+  const errors = verified ? [] : await verifyApm(pluginConfig, context);
+
+  try {
+    pkg = await getPkg(pluginConfig, context);
+  } catch (error) {
+    errors.push(...error);
+  }
+
+  if (errors.length > 0) {
+    throw new AggregateError(errors);
+  }
+
+  if (!prepared) {
+    await prepareApm(pluginConfig, context);
+    prepared = true;
+  }
+
+  return publishApm(pluginConfig, pkg, context);
+}
+
+module.exports = {verifyConditions, prepare, publish};
